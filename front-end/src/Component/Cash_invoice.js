c444c77dba3e69dba2cd9b1268615e1c33057026
Silapakarn Saniskosai <silapakaneragon@hotmail.co.th>
2022-06-21 00:23:09 +0700

    Update Front-end Layout

--- front-end/src/Component/Cash_invoice.js
+++ front-end/src/Component/Cash_invoice.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button } from 'antd';
 import '../Component/Cash_invoice.css';
-
+import { SiVerizon } from "react-icons/si";
 
 
 export default function Cash_invoice() {
 
 
-
+    
 
 
 
@@ -16,37 +16,47 @@ export default function Cash_invoice() {
     <div className="Header">
         <img src="https://flowaccount.com/blog/wp-content/uploads/2017/01/e27-flowaccount-logo.png" />
         <h1>สร้างใบเสร็จรับเงิน</h1>
-
-        <Button type="primary" htmlType="submit">ทดลองใช้งานฟรี</Button>
-        <Button type="primary" htmlType="submit">ลงชื่อเข้าใช้งาน</Button>
+        
+        <div className="Btn_Header">
+            <Button type="primary" htmlType="submit" className='button-65'>ทดลองใช้งานฟรี</Button>
+            <Button type="primary" htmlType="submit" className='button-65'>ลงชื่อเข้าใช้งาน</Button>
+        </div>
     </div>
 
 
     <div className="Content">
-            <div>
-                <Button type="primary" htmlType="submit">ส่ง</Button>
-                <Button type="primary" htmlType="submit">บันทึก</Button>
-                <Button type="primary" htmlType="submit">รับ PDF</Button>
-                <Button type="primary" htmlType="submit">พิมพ์</Button>
+            <div className="Btn_and_PDF">
+
+                <div className='Btn_Content'>
+                    <Button type="primary" htmlType="submit"  className='button-65'>ส่ง</Button>
+                    <Button type="primary" htmlType="submit"  className='button-65'>บันทึก</Button>
+                    <Button type="primary" htmlType="submit"  className='button-65'>รับ PDF</Button>
+                    <Button type="primary" htmlType="submit"  className='button-65'>พิมพ์</Button>
+                </div>
 
                 <div className="PDF">
-                    PDF
+                    <img src='https://edit.org/images/cat/invoices-big-2019042509.jpg'/>
                 </div>
             </div>
-        
-            <div>
-                <h3>ใส่ข้อมูลใบเสร็จรับเงิน</h3>
-                <h3>เพิ่มที่อยู่ลูกค้าของคุณ</h3>
-                <h3>เพิ่มข้อมูลสินค้า</h3>
-                <h3>พิมพ์ใบเสร็จรับเงิน</h3>
+
+            
+            <div className='Text_explain_procedure'>
+                <h2>ใส่ข้อมูลใบเสร็จรับเงิน</h2>
+                <p><SiVerizon /><span>เพิ่มที่อยู่ลูกค้าของคุณ</span></p>
+                <p><SiVerizon /><span>เพิ่มหมายเลขใบเสร็จรับเงิน</span></p>
+                <p><SiVerizon /><span>เพิ่มข้อมูลสินค้า</span></p>
+                <p><SiVerizon /><span>พิมพ์ใบเสร็จรับเงิน</span></p>
             </div>
     </div>
 
     <div className='Footer'>
         <div className='Sub_Footer'>
             <h2>ทดลองใช้โปรเเกรมบัญชี FlowAccount</h2>
-            <Button type="primary" htmlType="submit">สมัครใช้งาน</Button>
-            <Button type="primary" htmlType="submit">ติดต่อเรา</Button>
+
+            <div className='Btn_Footer'>
+                <Button type="primary" htmlType="submit" className='button-65'>สมัครใช้งาน</Button>
+                <Button type="primary" htmlType="submit" className='button-65'>ติดต่อเรา</Button>
+            </div>
         </div>
     </div>
     
